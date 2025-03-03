326e4fccef8c44b0b254b75d1a01572adcd13cb9
anamika222 <105983673+anamika222@users.noreply.github.com>
2025-03-03 20:31:20 +0600

    Add files via upload

---
+++ AboutPages.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const AboutPages = () => {
+    return (
+        <div>
+            <h2>This is AboutPage</h2>
+        </div>
+    );
+};
+export default AboutPages;
---
+++ HomePages.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner1 from "../Component/Banner1";
+import Accordation from "../Component/Accordation";
+
+const HomePages =() => {
+    return(
+        <div>
+            <Banner1></Banner1>
+            <Accordation></Accordation>
+            <Stat></Stat>
+        </div>
+    );
+};
+export default HomePages;
